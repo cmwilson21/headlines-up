ee948a2b265c3b06f66cd42173c76eb713b74c0c
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-23 21:38:55 -0500

    added very basic bones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, BrowserRouter as Router, Route } from "react-router";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={Home} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
