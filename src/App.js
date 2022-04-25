e4b9d2abad5a72d784cb9c83bf289dbe3b48346f
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-25 09:28:18 -0500

    added redux packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import { React, useState, useEffect } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/static/Home";
 import NavBar from "./components/static/NavBar";
 import { GameCard } from "./components/game/GameCard";
 import { About } from "./components/static/About";
+// import { API_KEY } from "./Globals";
+import { apiUrl } from "./Globals";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  // const apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news,fox-news,reuters,associated-press,cnn&apiKey=${API_KEY}`;
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(articles.articles);
+
   return (
     <div className="App">
       <Router>
--- src/Globals.js
+++ src/Globals.js
@@ -1,2 +1,2 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
-export const apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news,abc-news,fox-news,reuters,the-washington-post,politico,cbs-news,associated-press,cnn&apiKey=${API_KEY}`;
+export const apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news,fox-news,reuters,associated-press,cnn&apiKey=${API_KEY}`;
