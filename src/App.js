bc6c0086b238ed886f77d36b2756a82a54ba051f
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-25 09:08:20 -0500

    worked on navbar basics and css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 // import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/sessions/Home";
+import Home from "./components/static/Home";
+import NavBar from "./components/static/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ src/components/static/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <ul className="nav-ul">
+        {/* <div className="nav-li"> */}
+        <li className="nav-li">Home</li>
+        <li className="nav-li">Login</li>
+        <li className="nav-li">Sign Up</li>
+        <li className="nav-li">Play</li>
+        <li className="nav-li">About</li>
+        {/* </div> */}
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
