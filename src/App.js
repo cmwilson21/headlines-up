bb0c524629fe8a8366b934768083fdce4bb8ea76
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-25 09:13:44 -0500

    basic app.js routing added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/static/Home";
 import NavBar from "./components/static/NavBar";
+import { GameCard } from "./components/game/GameCard";
+import { About } from "./components/static/About";
 
 function App() {
   return (
@@ -12,6 +14,10 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* <Route path="/signup" element={<Home />} /> */}
+          {/* <Route path="/login" element={<Home />} /> */}
+          <Route path="/play" element={<GameCard />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <ul className="nav-ul">
-        {/* <div className="nav-li"> */}
-        <li className="nav-li">Home</li>
-        <li className="nav-li">Login</li>
-        <li className="nav-li">Sign Up</li>
-        <li className="nav-li">Play</li>
-        <li className="nav-li">About</li>
-        {/* </div> */}
+        <li className="nav-li">
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li className="nav-li">
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li className="nav-li">
+          <NavLink to="/signup">Sign Up</NavLink>
+        </li>
+        <li className="nav-li">
+          <NavLink to="/play">Play</NavLink>
+        </li>
+        <li className="nav-li">
+          <NavLink to="/about">About</NavLink>
+        </li>
       </ul>
     </div>
   );
