457efa39d89fcff860ff39e99e895777303b33a7
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-25 08:58:57 -0500

    fixed component not loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, BrowserRouter as Router, Route } from "react-router";
-import Home from "./components/Home";
+// import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/sessions/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={Home} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
