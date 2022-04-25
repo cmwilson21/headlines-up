56abed64409de90cb79434c978f2693419ebb6a0
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-25 17:32:31 -0500

    added login and signup components

--- src/App.js
+++ src/App.js
@@ -9,26 +9,17 @@ import { About } from "./components/static/About";
 import { apiUrl } from "./Globals";
 import { useDispatch, useSelector } from "react-redux";
 import { loadArticle } from "./actions/articleAction";
+import Signup from "./components/sessions/Signup";
+import Login from "./components/sessions/Login";
 
 function App() {
-  // const [articles, setArticles] = useState([]);
   const dispatch = useDispatch();
   const article = useSelector((state) => state.articles);
   const requesting = useSelector((state) => state.requesting);
 
-  // use redux to fetch the articles from apiurl
   useEffect(() => {
     dispatch(loadArticle());
   }, []);
-  // console.log("articles", articles);
-
-  // useEffect(() => {
-  //   fetch(apiUrl)
-  //     .then((response) => response.json())
-  //     .then((data) => setArticles(data.articles))
-  //     .catch((error) => console.log(error));
-  // }, []);
-  // console.log(articles.articles);
 
   return (
     <div className="App">
@@ -36,8 +27,8 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/signup" element={<Home />} /> */}
-          {/* <Route path="/login" element={<Home />} /> */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/play" element={<GameCard />} />
           <Route path="/about" element={<About />} />
         </Routes>
