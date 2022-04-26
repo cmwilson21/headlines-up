130bb6f790ce41164dce76aeafdd556597b83672
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-26 08:28:27 -0500

    cleaned up unused comments

--- src/components/game/GameCard.js
+++ src/components/game/GameCard.js
@@ -13,28 +13,11 @@ export const GameCard = () => {
   }, [dispatch]);
   console.log("articles", articles);
 
-  // const randomArticle = articles[Math.floor(Math.random() * articles.length)];
-
-  // create and array of random articles from the api
-  // const randomArticles = [];
-  // for (let i = 0; i < 3; i++) {
-  //   randomArticles.push(articles[Math.floor(Math.random() * articles.length)]);
-  // }
-
-  // console.log("randomArticles", randomArticles);
-
-  // const singleArticle = articles[0];
-  // console.log("article2", article);
-  // const randomArticle =
-  //   articles.articles[Math.floor(Math.random() * articles.articles.length)];
-  // console.log("rando", randomArticle);
   if (articles) {
     return (
       <div>
         <div className="card">
           <div className="card-body">
-            {/* <h5 className="card-title">{randomArticle.title}</h5>
-          <p className="card-text">{randomArticle.body}</p> */}
             <h5 className="card-title">{articles.title}</h5>
             <p className="card-text">{articles.description}</p>
             <p>{articles.content}</p>
