d350cabdc0431e019eb5badfd900a5a5885453d2
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-28 23:20:19 -0500

    basic sign up form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/static/Home";
 import NavBar from "./components/static/NavBar";
 import { GameCard } from "./components/game/GameCard";
 import { About } from "./components/static/About";
+import Signup from "./components/sessions/Signup";
 // import { apiUrl } from "./Globals";
 // import { useDispatch, useSelector } from "react-redux";
 // import { loadArticle } from "./actions/articleAction";
@@ -26,7 +27,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/signup" element={<Home />} /> */}
+          <Route path="/signup" element={<Signup />} />
           {/* <Route path="/login" element={<Home />} /> */}
           <Route path="/play" element={<GameCard />} />
           <Route path="/about" element={<About />} />
--- src/components/sessions/Signup.js
+++ src/components/sessions/Signup.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  // create a signup form with first name, last name, email, and password
+  // create a signup button that will send the user to the login page
+  const SignupForm = () => {
+    return (
+      <div>
+        <h1>Signup</h1>
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="firstName" />
+          </label>
+          <br />
+          <label>
+            Last Name:
+            <input type="text" name="lastName" />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="text" name="email" />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+          <br />
+          <button type="submit">Signup</button>
+        </form>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
 };
 
 export default Signup;
