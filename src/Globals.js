afd01e521ba8910c482f426bc30fab4fad91d115
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-04-30 13:51:28 -0500

    starting vercel server

--- src/Globals.js
+++ src/Globals.js
@@ -1,3 +1,3 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
-export const apiUrl = `http://localhost:3010/proxy/v2/top-headlines?sources=bbc-news,fox-news,reuters,associated-press,cnn&apiKey=${API_KEY}`;
+export const apiUrl = `//proxy/v2/top-headlines?sources=bbc-news,fox-news,reuters,associated-press,cnn&apiKey=${API_KEY}`;
 // export const apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news,fox-news,reuters,associated-press,cnn&apiKey=${API_KEY}`;
