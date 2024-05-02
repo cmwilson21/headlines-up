944071eec797ee953c596727d1c566c46f935013
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-05-02 16:08:54 -0500

    stored congrats card in its own file

--- src/components/game/GameCard.js
+++ src/components/game/GameCard.js
@@ -1,34 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadArticle } from "../../actions/articleAction";
-
-const CongratsCard = ({ article }) => {
-  return (
-    <div>
-      <div className="congrats-card">
-        <div className="congrats-card-body">
-          <h5 className="congrats-card-title text-green">Congratulations!</h5>
-          <p className="congrats-card-text">You answered correctly!</p>
-          <img
-            src={article.urlToImage}
-            alt=""
-            component="img"
-            height="250"
-            width="auto"
-          />
-          <h5>{article.title}</h5>
-          <p>{article.description}</p>
-          <p>
-            {article.content}...{" "}
-            <a href={article.url} target="_blank" rel="noreferrer">
-              view full article.
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+import CongratsCard from "./CongratsCard.js";
 
 const CardBody = ({ article, nextButton }) => {
   const [selection, setSelection] = useState("");
