4916783df90fb61db6391b46bfe66f84cf6952ed
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-05-02 16:04:32 -0500

    adding congrats card component

---
+++ src/components/game/CongratsCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const CongratsCard = ({ article }) => {
+  return (
+    <div>
+      <div className="congrats-card">
+        <div className="congrats-card-body">
+          <h5 className="congrats-card-title text-green">Congratulations!</h5>
+          <p className="congrats-card-text">You answered correctly!</p>
+          <img
+            src={article.urlToImage}
+            alt=""
+            component="img"
+            height="250"
+            width="auto"
+          />
+          <h5>{article.title}</h5>
+          <p>{article.description}</p>
+          <p>
+            {article.content}...{" "}
+            <a href={article.url} target="_blank" rel="noreferrer">
+              view full article.
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CongratsCard;
