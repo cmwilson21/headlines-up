d4334fe75ac523a78ef42d5556b9a9f2732360fe
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-05-07 20:35:43 -0500

    added express login and signup routing - tested with postman

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const proxy = require("express-http-proxy");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3010;
+const userStore = new Map();
+userStore.set("bob", { password: "123", score: 0 });
 
 app.use("/proxy", proxy("https://newsapi.org"));
+
+app.post("/login", bodyParser.json(), (req, res) => {
+  const user = userStore.get(req.body.username);
+  if (user && user.password === req.body.password) {
+    res.send(200);
+  } else {
+    res.send(401);
+  }
+  // console.log(req.body.username);
+});
+
+app.put("/signup", bodyParser.json(), (req, res) => {
+  const user = userStore.get(req.body.username);
+  if (user) {
+    res.send(401);
+  } else {
+    userStore.set(req.body.username, { password: req.body.password, score: 0 });
+    res.send(200);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
