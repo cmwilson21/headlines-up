712c9b13665d966f6ea63248275c8a6a7b7ddbcf
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-06-15 21:27:26 -0500

    commiting changes to database struture for signup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const proxy = require("express-http-proxy");
 const jwt = require("jsonwebtoken");
@@ -8,25 +9,57 @@ const port = 3010;
 const userStore = new Map();
 userStore.set("bob", { password: "123", score: 0 });
 
+// Open a database handle
+let db = new sqlite3.Database(
+  "./users.db",
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the users database.");
+  }
+);
+
+// Create table (if not exists)
+db.run(`CREATE TABLE IF NOT EXISTS users (
+  username TEXT PRIMARY KEY,
+  password TEXT NOT NULL,
+  score INTEGER
+)`);
+
 app.use("/proxy", proxy("https://newsapi.org"));
 
-app.post("/login", bodyParser.json(), (req, res) => {
-  const user = userStore.get(req.body.username);
-  if (user && user.password === req.body.password) {
-    res.send(200);
-  } else {
-    res.send(401);
-  }
+app.post("/login", bodyParser.json(), (req, res, next) => {
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [req.body.username],
+    (err, row) => {
+      if (err) {
+        return next(err);
+      }
+      if (row && row.password === req.body.password) {
+        res.send(200);
+      } else {
+        res.send(401);
+      }
+    }
+  );
 });
 
-app.put("/signup", bodyParser.json(), (req, res) => {
-  const user = userStore.get(req.body.username);
-  if (user) {
-    res.send(401);
-  } else {
-    userStore.set(req.body.username, { password: req.body.password, score: 0 });
-    res.send(200);
-  }
+app.put("/signup", bodyParser.json(), (req, res, next) => {
+  console.log(req.body);
+  db.run(
+    "INSERT INTO users(username, password, score) VALUES(?, ?, ?)",
+    [req.body.username, req.body.password, 0],
+    function (err) {
+      if (err) {
+        next(err);
+        return console.log(err.message);
+      }
+      res.sendStatus(200);
+    }
+  );
 });
 
 app.listen(port, () => {
