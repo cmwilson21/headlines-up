56919d892d407a0974584fcdb4ca1037d99cf0fd
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-06-30 07:05:30 -0500

    added styling to game cards

--- src/components/game/CongratsCard.js
+++ src/components/game/CongratsCard.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./game-components.css";
 
 // The congrats card is displayed when the user answers correctly.
 
 const CongratsCard = ({ article }) => {
   return (
-    <div>
+    <div className="card-container">
       <div className="congrats-card">
         <div className="congrats-card-body">
           <h5 className="congrats-card-title text-green">Congratulations!</h5>
@@ -16,9 +17,9 @@ const CongratsCard = ({ article }) => {
             height="250"
             width="auto"
           />
-          <h5>{article.title}</h5>
-          <p>{article.description}</p>
-          <p>
+          <h5 className="article-title">{article.title}</h5>
+          <p className="article-text">{article.description}</p>
+          <p className="article-text">
             {article.content}...{" "}
             <a href={article.url} target="_blank" rel="noreferrer">
               view full article.
